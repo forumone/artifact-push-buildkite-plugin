40c4b2494ae3e4e2aa13a02d731b751ad8899eb1
Greg Wilson <gwilson@forumone.com>
2026-05-11 10:37:32 -0700

    do not use /tmp for target

--- lib/files.bash
+++ lib/files.bash
@@ -7,5 +7,5 @@
 #
 # Creates a temporary directory based on the Buildkite environment.
 target-directory() {
-  mktemp --directory "$BUILDKITE_PIPELINE_SLUG-$BUILDKITE_BRANCH-$BUILDKITE_BUILD_NUMBER-tmp.XXXXXXXXXX"
+  mktemp --tmpdir=/var --directory "$BUILDKITE_PIPELINE_SLUG-$BUILDKITE_BRANCH-$BUILDKITE_BUILD_NUMBER-tmp.XXXXXXXXXX"
 }
